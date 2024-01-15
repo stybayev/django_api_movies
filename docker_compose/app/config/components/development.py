bb8f80ba58931f620a901eef7071e124ce0b985f
dosxana@gmail.com <dosxana@gmail.com>
2024-01-15 12:55:27 +0600

    Спринт 3/16: 2: Сервис Admin Panel - Релизовал отдачу статики через nginx

---
+++ docker_compose/app/config/components/development.py
@@ -0,0 +1,6 @@
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
---
+++ docker_compose/app/config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
