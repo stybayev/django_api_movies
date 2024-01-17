d548c96d0d0842de3378706bb01406292d6af6c4
dosxana@gmail.com <dosxana@gmail.com>
2024-01-17 18:24:44 +0600

    Спринт 3/15: 2: Сервис Admin Panel - Реализовал оптимизацию запросов через prefech_related в представлениях FilmWorkList, FilmWorkDetail

--- django_api/app/movies/api/v1/serializers.py
+++ django_api/app/movies/api/v1/serializers.py
@@ -1,19 +1,43 @@
 from rest_framework import serializers
-
 from movies.models import FilmWork, Person, Genre
 
+from movies.models import PersonRoleType
 
-class FilmWorkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FilmWork
-        fields = '__all__'
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = '__all__'
 
+
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = '__all__'
+
+
+class FilmWorkSerializer(serializers.ModelSerializer):
+    genres = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='id'
+    )
+    actors = serializers.SerializerMethodField()
+    directors = serializers.SerializerMethodField()
+    writers = serializers.SerializerMethodField()
+
+    class Meta:
+        model = FilmWork
+        fields = '__all__'
+
+    def get_actors(self, obj):
+        # Возвращает список идентификаторов актеров для фильма
+        return obj.persons.filter(personfilmwork__role=PersonRoleType.ACTOR.value).values_list('id', flat=True)
+
+    def get_directors(self, obj):
+        # Возвращает список идентификаторов режиссеров для фильма
+        return list(obj.persons.filter(personfilmwork__role=PersonRoleType.DIRECTOR.value).values_list('id', flat=True))
+
+    def get_writers(self, obj):
+        # Возвращает список идентификаторов сценаристов для фильма
+        return list(obj.persons.filter(personfilmwork__role=PersonRoleType.WRITER.value).values_list('id', flat=True))
--- django_api/app/movies/api/v1/views.py
+++ django_api/app/movies/api/v1/views.py
@@ -1,22 +1,37 @@
-from rest_framework import generics, serializers
-
+from rest_framework import generics
 from movies.models import FilmWork
-
-from movies.models import Person, Genre
 from .serializers import FilmWorkSerializer
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 
-class GenreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Genre
-        fields = '__all__'
+class CustomPageNumberPagination(PageNumberPagination):
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'next': self.get_next_link(),
+            'prev': self.get_previous_link(),
+            'results': data
+        })
+
+    def get_next_link(self):
+        if not self.page.has_next():
+            return None
+        return self.page.next_page_number()
+
+    def get_previous_link(self):
+        if not self.page.has_previous():
+            return None
+        return self.page.previous_page_number()
 
 
 class FilmWorkList(generics.ListAPIView):
-    queryset = FilmWork.objects.all()
+    queryset = FilmWork.objects.prefetch_related('genres', 'persons').all()
     serializer_class = FilmWorkSerializer
+    pagination_class = CustomPageNumberPagination
 
 
 class FilmWorkDetail(generics.RetrieveAPIView):
-    queryset = FilmWork.objects.all()
+    queryset = FilmWork.objects.prefetch_related('genres', 'persons').all()
     serializer_class = FilmWorkSerializer
