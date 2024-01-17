09042a85a48f4739f4fb35f13255e217ca703404
dosxana@gmail.com <dosxana@gmail.com>
2024-01-17 15:46:16 +0600

    Спринт 3/15: 2: Сервис Admin Panel - Изменил настройки cors в проекте

--- django_api/app/config/components/cors.py
+++ django_api/app/config/components/cors.py
@@ -1,24 +1,29 @@
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:*'
-    'http://localhost:*',
+    'http://localhost:8000',
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:*',
-    'http://localhost:*',
+    'http://localhost:8000',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8000',
+)
 
 CSRF_COOKIE_SECURE = False
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_HEADERS = ('content-disposition',
-                      'accept-encoding',
-                      'content-type',
-                      'accept',
-                      'origin',
-                      'authorization',
-                      'x-api-key',
-                      )
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
--- django_api/app/config/components/middleware.py
+++ django_api/app/config/components/middleware.py
@@ -1,8 +1,8 @@
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
